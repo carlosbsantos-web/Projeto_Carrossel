b9b31435d7d8846c4add4677e6cd57574f43f150
Carlos <carlos@gamil.com>
2024-08-30 14:57:25 -0300

    iniciando repositório

---
+++ js/script.js
@@ -0,0 +1,63 @@
+
+// class Person {
+
+//     constructor(nome, sobrenome, idade) {
+
+//         this.nome = nome,
+//         this.sobrenome = sobrenome,
+//         this.idade = idade
+
+
+//     }
+
+//     nomeCompleto = () => {
+
+//         return `Olá sou ${this.nome} e meu sobrenone é ${this.sobrenome} ${this.idade}`
+
+//     }
+// };
+
+// const elemento = new Person('Carlos', 'Santos', 39);
+// const elemento1= new Person('Ana', 'Maria', 21);
+// const elemento2 = new Person('Bruna', 'Paula', 19);
+// console.log(elemento.nomeCompleto());
+// console.log(elemento.nomeCompleto());
+// console.log(elemento.nomeCompleto());
+
+// const pessoas = {
+    
+//     nome: 'Carlos', 
+//     sobrenome: 'Santos', 
+//     idade: 39,
+
+//     dadosCompleto: function() {
+ 
+//         return this.nome + " " + this.sobrenome + " " + this.idade
+//     }
+
+// };
+
+// // document.querySelector('#demo').innerHTML = pessoas.dadosCompleto();
+
+// console.log(pessoas.dadosCompleto());
+
+// const pessoas = ('Carlos Batista')
+// console.log(pessoas.replace('Batista', 'Santos'));
+
+const number = 12;
+
+if( number != 12) {
+
+    try {
+
+console.log('Bom dia')
+
+    
+    } catch (erro) {
+
+        console.log('Erro ao entrar: Código 182')
+
+    }
+
+};
+
